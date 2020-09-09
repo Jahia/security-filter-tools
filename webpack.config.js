d2690f5c40fd1f2eccb7243f5242a14e2cd0676a
Yousria <ybenchadi@jahia.com>
2020-09-09 10:13:04 +0200

    BACKLOG-14534 : add JWT Configuration in developerTools panel

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,68 @@
 var path = require('path');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 var env = process.env.WEBPACK_ENV || 'development';
 
-module.exports = {
-    entry: path.resolve(__dirname, 'src/javascript', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'src/main/resources/javascript/apps/'),
-        filename: 'security-filter-jwt.js'
-    },
-    resolve: {
-        mainFields: ['module', 'main'],
-        extensions: ['.mjs', '.js', '.jsx', 'json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.mjs$/,
-                include: /node_modules/,
-                type: "javascript/auto",
-            },
-            {
-                test: /\.jsx?$/,
-                include: [path.join(__dirname, "src")],
-                loader: 'babel-loader',
+// Get manifest
+var normalizedPath = require('path').join(__dirname, './target/dependency');
+var manifest = '';
+require('fs').readdirSync(normalizedPath).forEach(function (file) {
+    manifest = './target/dependency/' + file;
+    console.log('use manifest ' + manifest);
+});
 
-                query: {
-                    presets: [['env', {modules: false}], 'react', 'stage-2'],
-                    plugins: [
-                        "lodash"
-                    ]
+module.exports = (env, argv) => {
+    const config = {
+        entry: {
+            main: [path.resolve(__dirname, 'src/javascript/publicPath'), path.resolve(__dirname, 'src/javascript/index.js')]
+        },
+        output: {
+            path: path.resolve(__dirname, 'src/main/resources/javascript/apps/'),
+            filename: 'jahia.bundle.js'
+        },
+        resolve: {
+            mainFields: ['module', 'main'],
+            extensions: ['.mjs', '.js', '.jsx', 'json']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.mjs$/,
+                    include: /node_modules/,
+                    type: 'javascript/auto',
+                },
+                {
+                    test: /\.jsx?$/,
+                    include: [path.join(__dirname, "src")],
+                    loader: 'babel-loader',
+
+                    query: {
+                        presets: [['env', {modules: false}], 'react', 'stage-2'],
+                        plugins: [
+                            'lodash'
+                        ]
+                    }
                 }
-            }
-        ]
-    },
-    mode: env,
-    devtool: 'source-map'
+            ]
+        },
+        plugins: [
+            new webpack.DllReferencePlugin({
+                manifest: require(manifest)
+            }),
+            new webpack.HashedModuleIdsPlugin({
+                hashFunction: 'sha256',
+                hashDigest: 'hex',
+                hashDigestLength: 20
+            })
+        ],
+        mode: 'development'
+    };
+    config.devtool = (argv.mode === 'production') ? 'source-map' : 'eval-source-map';
+
+    if (argv.analyze) {
+        config.devtool = 'source-map';
+        config.plugins.push(new BundleAnalyzerPlugin());
+    }
+
+    return config;
 };
