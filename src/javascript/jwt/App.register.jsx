60408e2ac4bc5930561e8e29d0a82743ba4846b6
Thomas Draier <tdraier@jahia.com>
2021-09-29 16:53:28 +0200

    BACKLOG-15979: Upgrade libs: (#20)

--- src/javascript/jwt/App.register.jsx
+++ src/javascript/jwt/App.register.jsx
@@ -1,16 +1,18 @@
 import React, {Suspense} from 'react';
 import {registry} from '@jahia/ui-extender';
-import {Jwt} from '@jahia/moonstone';
-import {ProgressPaper} from '@jahia/design-system-kit';
+import {Jwt,Loader} from '@jahia/moonstone';
 
-window.jahia.i18n.loadNamespaces('security-filter-tools');
-const Component = React.lazy(() => import('./App'));
 
-registry.add('adminRoute', 'jwtConfiguration', {
-    targets: ['developerTools:40'],
-    requiredPermission: 'developerToolsAccess',
-    icon: <Jwt/>,
-    label: 'security-filter-tools:jwtConfiguration',
-    isSelectable: true,
-    render: () => <Suspense fallback={<ProgressPaper/>}><Component/></Suspense>
-});
+export default function () {
+    window.jahia.i18n.loadNamespaces('security-filter-tools');
+    const Component = React.lazy(() => import('./App'));
+
+    registry.add('adminRoute', 'jwtConfiguration', {
+        targets: ['developerTools:40'],
+        requiredPermission: 'developerToolsAccess',
+        icon: <Jwt/>,
+        label: 'security-filter-tools:jwtConfiguration',
+        isSelectable: true,
+        render: () => <Suspense fallback={<Loader/>}><Component/></Suspense>
+    });
+}
