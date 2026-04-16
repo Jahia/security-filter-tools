9ba78eb2a0754abf68c214aa0d5b6928a68172b8
David Griffon <dgriffon@jahia.com>
2026-04-16 15:57:37 +0200

    fix: add missing import + publish workflow on merge (#72)

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -1,7 +1,8 @@
 import {registry} from '@jahia/ui-extender';
 import register from './jwt/App.register';
 // Ensure @apollo/react-hooks is bundled as a shared Module Federation dependency
-// eslint-disable next-line no-undef
+// eslint-disable-next-line no-unused-vars
+import {useQuery} from '@apollo/react-hooks'; // NOSONAR
 
 export default function () {
     registry.add('callback', 'securityfiltertools', {
